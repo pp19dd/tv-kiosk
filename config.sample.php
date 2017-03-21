575bd15679de314b1955c5611425188f52908ee0
pp19dd <pp19dd@gmail.com>
2017-03-21 16:31:10 -0400

    Config sample updated to reflect dev mode

---
+++ config.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// live mode
+define( "FEED", "http://example.com/json" );
+
+// development mode
+
+// instructions: save a copy of the feed locally as data.json and point`
+// the FEED constant to it, such as:
+//      define( "FEED", "data.json" );
