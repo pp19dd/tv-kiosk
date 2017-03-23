64b7ce5ac43614fa6dab1c485e576d09e300b11b
Stephen Mekosh <smekosh@DC-IMAC3454-001.local>
2017-03-23 19:12:42 -0400

    Added code for video player; added publication datetime to listings

--- home.php
+++ home.php
@@ -14,16 +14,32 @@ $all_videos = get_feed();
 
     <header id="page-header">
         <div class="inner">
-            <h1>VOA Kiosk</h1>
+            <h1><a rel="home" href="http://www.voanews.com/">VOA</a></h1>
         </div>
     </header>
-
+    
+    
+    
     <main id="main" class="inner">
 
         <section id="playing">
-            <h2>Currently playing</h2>
+            <section class="player">
+                
+                <?php 
+                // grabs a random video to display
+                $all_cat = array_keys($all_videos);
+                shuffle($all_cat);
+                $any_vid = $all_videos[$all_cat[0]][0];
+                ?>
+                
+                <div class="video-wrapper"><iframe src="<?php echo $any_vid->embed_url; ?>" width="640" height="363"></iframe></div>
+                
+                <h1><?php echo $any_vid->Title; ?></h1>
+                <p><?php echo $any_vid->Description; ?></p>
+                
+            </section>
         </section>
-
+        
         <section id="listings">
 
             <?php foreach( $all_videos as $category => $videos ) { ?>
@@ -32,12 +48,15 @@ $all_videos = get_feed();
             <ul class="videos">
             <?php foreach( $videos as $video ) { ?>
 
-            <li class="video">
-                <a href="<?php echo $video->Link ?>">
+            <li class="video" id="v<?php echo $video->pangea_id; ?>">
+                <a href="<?php echo $video->Link ?>" data-video="<?php echo $video->pangea_id; ?>">
                     <h3><?php echo $video->Title ?></h3>
                     <div class="video-still"><img src="<?php echo pangea_image( $video->Image, 300 ); ?>" /></div>
-                    <p class="description"><?php echo $video->Description ?></p>
+
+<!-- TODO: REPLACE WITH MORE SOPHISTICATED TRUNCATION -->
+                    <p class="description"><?php echo mb_substr( $video->Description, 0, 250 ); ?></p>
                 </a>
+                <time class="published" datetime="<?php echo date( 'Y-m-d\TH:i', strtotime($video->PublicationDate) ); ?>"><?php echo date( 'F j, g:i a, T', strtotime($video->PublicationDate) ); ?></time>
             </li>
 
             <?php } # videos ?>
@@ -48,7 +67,9 @@ $all_videos = get_feed();
         </section>
 
     </main>
-
+    
+    
+    
     <footer>VOA footer...</footer>
 
 </body>
